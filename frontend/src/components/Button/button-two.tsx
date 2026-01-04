c7b3041330d2ee1f59724bbd810fc7ce29147c02
Ben Parisot <me@benparisot.com>
2026-01-04 10:59:44 -0800

    sets up scrubby and adds violation component

---
+++ frontend/src/components/Button/button-two.tsx
@@ -0,0 +1,15 @@
+import {React} from 'react';
+
+class SecondButton extends React.Component {
+
+  render() {
+    return (
+      <>
+        <p>this is a second button</p>
+        <button>Button 2</button>
+      </>
+    )
+  }
+}
+
+export default SecondButton;
