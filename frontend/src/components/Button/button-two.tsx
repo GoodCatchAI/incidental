020089087fd2962df0f76d42d4e2b3c8bdcf85af
Ben Parisot <me@benparisot.com>
2026-01-04 14:10:15 -0800

    tests out the renamed scrubby extension for breaks

--- frontend/src/components/Button/button-two.tsx
+++ frontend/src/components/Button/button-two.tsx
@@ -5,6 +5,7 @@ class SecondButton extends React.Component {
   render() {
     return (
       <>
+        <h1>hello there world</h1>
         <p>this is a second button</p>
         <button>Button 2</button>
       </>
